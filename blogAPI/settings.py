2690343094fb1140647c348b571144a546b818e9
mucheru-delvan <mkdelvan9@gmail.com>
2026-08-08 20:54:24 +0300

    Created a BlogPost model

--- blogAPI/settings.py
+++ blogAPI/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blogAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'bloguser',
+        'PASSWORD': 'frazier',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
