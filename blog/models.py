770a04a5bce2100ba24046092ba2578c390d7b26
mucheru-delvan <mkdelvan9@gmail.com>
2026-08-10 08:17:33 +0300

    Created a tag class for manytomany relationship

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,35 @@ from django.db import models
 
 
 # Create your models here.
-class BlogPost(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        null=True, 
+        blank=True, 
+        related_name='posts'
+        )
+    
+    tags = models.ManyToManyField(
+        Tag, 
+        related_name='posts'
+        )
+        
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
