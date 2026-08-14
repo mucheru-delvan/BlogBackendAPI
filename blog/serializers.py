f7a9fe76dd5f663f7aa3e3a63f6b10a3b58d439b
mucheru-delvan <mkdelvan9@gmail.com>
2026-08-14 08:23:13 +0300

    Installed python decouple to hash the passwords

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Tag, Category
+
 
 class PostSerializer(serializers.ModelSerializer):
+
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
 
     class Meta:
         model = Post
@@ -11,6 +21,17 @@ class PostSerializer(serializers.ModelSerializer):
             "content",
             "category",
             "tags",
+            "created_at",
+            "updated_at",
         ]
-        
-        
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        representation["category"] = instance.category.name
+
+        representation["tags"] = [
+            tag.name for tag in instance.tags.all()
+        ]
+
+        return representation
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-
+from django.db.models import Q
 from .models import Post
 from .serializers import PostSerializer
 
@@ -8,6 +8,13 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
-    
+    def get_queryset(self):
+        queryset = Post.objects.all()
         
-        
+        term = self.request.query_params.get("term")
+        if term:
+            queryset = queryset.filter(
+                Q(title__icontains=term) |
+                Q(content__icontains=term) | 
+                Q(category__name__icontains=term))
+        return queryset
